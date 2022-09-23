e14452ba993fe7091410bccf82c7ee8c300b0ee9
WEB/BLOCKCHAIN WORLD <92005974+topcoder728@users.noreply.github.com>
2022-09-23 10:31:03 +0900

    feat: complete

--- src/config/site.config.js
+++ src/config/site.config.js
@@ -1,9 +1,9 @@
 export default {
-  CONTRACT_ADDRESS: "0xDD3AffCF20ce2f67C3452c1e064b57a70F13fa54",
+  CONTRACT_ADDRESS: "0x0ca5CBfAf5CbF9fe48a06049C5b706a90a85DFB1", //"0xDD3AffCF20ce2f67C3452c1e064b57a70F13fa54",
   NETWORK: {
-    NAME: "Ethereum Mainnet",
-    SYMBOL: "ETH",
-    ID: 1
+    NAME: "Mumbai", //"Ethereum Mainnet",
+    SYMBOL: "Matic", //"ETH",
+    ID: 80001 //1
   },
   DISPLAY_COST: 0.1,
 }
